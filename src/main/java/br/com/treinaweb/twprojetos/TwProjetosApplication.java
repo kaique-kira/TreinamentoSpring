2be9d1b40328f9af5c5ad1d05a3525296d52fcea
Kaique Kira <83593559+kaique-kira@users.noreply.github.com>
2022-07-20 16:50:46 -0300

    First Commit
    Criando o Repository do treinamento

---
+++ src/main/java/br/com/treinaweb/twprojetos/TwProjetosApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.twprojetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwProjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwProjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/treinaweb/twprojetos/TwProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.twprojetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

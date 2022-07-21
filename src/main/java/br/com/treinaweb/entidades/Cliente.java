07c399bd63338d058cdf1aece83c8506b6cb4e1a
Kaique Kira <83593559+kaique-kira@users.noreply.github.com>
2022-07-20 21:27:46 -0300

    <feat> - criando a Camada Repository  e a Entity Cliente

---
+++ src/main/java/br/com/treinaweb/entidades/Cliente.java
@@ -0,0 +1,36 @@
+package br.com.treinaweb.entidades;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 80)
+    private String nome;
+
+    @Column(nullable = false, length = 14, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, length = 15)
+    private String telefone;
+
+    @Column(nullable = false, length = 80, unique = true)
+    private String email;
+
+    @Column(name = "data_nascimento", nullable = false)
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    private LocalDate dataNascimento;
+}
